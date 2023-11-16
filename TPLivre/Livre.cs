c753078cbae6d0115c08bd99c5cc26b2553ec399
WaitInTheBush <theo.ollivier@gmail.com>
2023-11-16 09:36:00 +0100

    Ajoutez des fichiers projet.

---
+++ TPLivre/Livre.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLivre
+{
+    /// <summary>
+    /// Class représenant un livre
+    /// 3 attributs : titre, auteur et prix
+    /// </summary>
+    public class Livre
+    {
+        private string _Titre;
+        private string _Auteur;
+        private decimal _Prix;
+
+        /// <summary>
+        /// Titre du livre
+        /// </summary>
+        public string Titre { get => _Titre; set => _Titre = value; }
+        /// <summary>
+        /// Auteur du livre (Nom complet)
+        /// </summary>
+        public string Auteur { get => _Auteur; set => _Auteur = value; }
+        /// <summary>
+        /// Prix du livre
+        /// </summary>
+        public decimal Prix { get => _Prix; set => _Prix = value; }
+
+        /// <summary>
+        /// Constructeur de Livre (livre vide)
+        /// </summary>
+        public Livre()
+        {
+
+        }
+        /// <summary>
+        /// Constructeur de livre sans le prix
+        /// </summary>
+        /// <param name="titre">Titre du livre</param>
+        /// <param name="auteur">Auteur du livre</param>
+        public Livre(string titre, string auteur)
+        {
+            Titre = titre;
+            Auteur = auteur;
+        }
+        /// <summary>
+        /// Constructeur complet de livre 
+        /// </summary>
+        /// <param name="titre">Titre du livre</param>
+        /// <param name="auteur">Auteur du livre</param>
+        /// <param name="prix">Prix du livre</param>
+        public Livre(string titre, string auteur, decimal prix)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            Prix = prix;
+        }
+
+        /// <summary>
+        /// Permet de récupérer le titre du livre
+        /// </summary>
+        /// <returns>Titre du livre</returns>
+        public string GetTitre()
+        {
+            return _Titre;
+        }
+        /// <summary>
+        /// Permet de modifier le titre du livre
+        /// </summary>
+        /// <param name="titre">nouveau titre du livre</param>
+        public void SetTitre(string titre)
+        {
+            _Titre = titre;
+        }
+        /// <summary>
+        /// Permet d'obtenir l'auteur du livre
+        /// </summary>
+        /// <returns>Auteur du livre</returns>
+        public string GetAuteur()
+        {
+            return _Auteur;
+        }
+        /// <summary>
+        /// Permet de modifier l'auteur du livre
+        /// </summary>
+        /// <param name="auteur">Nouvel auteur du livre</param>
+        public void SetAuteur(string auteur)
+        {
+            _Auteur = auteur;
+        }
+
+        /// <summary>
+        /// Permet d'obtenir le prix du livre
+        /// </summary>
+        /// <returns>Prix du livre</returns>
+        public decimal GetPrix()
+        {
+            return _Prix;
+        }
+        /// <summary>
+        /// Permet de modifier le prix du livre
+        /// </summary>
+        /// <param name="prix">Nouveau prix du livre</param>
+        public void SetPrix(decimal prix)
+        {
+            _Prix = prix;
+        }
+        /// <summary>
+        /// Permet d'afficher toutes les informations d'un livre
+        /// </summary>
+        public void Afficher()
+        {
+            Console.WriteLine($"Titre : {Titre}");
+            Console.WriteLine($"Auteur : {Auteur}");
+            Console.WriteLine($"Prix : {Prix}");
+        }
+    }
+}
---
+++ TPLivre/Program.cs
@@ -0,0 +1,46 @@
+﻿
+//    Définir une classe Livre avec les attributs suivants : Titre, Auteur (Nom complet), Prix.
+//    Définir à l’aide des propriétés les méthodes d’accès aux différents attributs de la classe.
+//    Définir un constructeur permettant d’initialiser les attributs d'une instance par des valeurs saisies par l’utilisateur.
+//    Définir la méthode Afficher ( ) permettant d’afficher les informations du livre en cours.
+//    Écrire un programme testant la classe Livre.
+
+using TPLivre;
+
+// création d'une liste de livre
+List<Livre> Livres = new List<Livre>();
+
+// 1er livre
+Livre L1 = new Livre();
+Livres.Add(L1);
+Console.WriteLine("Titre du livre : ");
+string titre = Console.ReadLine();
+Console.WriteLine("Auteur du livre : ");
+string auteur = Console.ReadLine();
+Console.WriteLine("Prix du livre : ");
+decimal prix = decimal.Parse(Console.ReadLine());
+
+L1.SetTitre(titre);// avec le getter
+L1.Titre = titre; // avec la properties
+
+L1.Auteur = auteur;
+L1.Prix = prix;
+
+
+
+// 2eme livre
+
+Console.WriteLine("Titre du livre : ");
+titre = Console.ReadLine();
+Console.WriteLine("Auteur du livre : ");
+auteur = Console.ReadLine();
+Console.WriteLine("Prix du livre : ");
+prix = decimal.Parse(Console.ReadLine());
+
+Livre L2 = new Livre(titre,auteur,prix);
+Livres.Add(L2);
+
+foreach (Livre livre in Livres)
+{
+    livre.Afficher();
+}
